2ba6d3379d5028298dd6aefe211450ac71b960dc
Bailey Johnson <baileywjohnson@gmail.com>
2021-03-21 13:51:03 -0500

    Removing CRA Bloat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <div className="row justify-content-center">
+        <h1>MapQueue</h1>
+      </div>
+      <div className="row justify-content-center">
+        <p className="my-2 text-center">MapQueue is a service that allows users to create queues of beatmaps for "osu!".</p>
+      </div>
     </div>
   );
 }
